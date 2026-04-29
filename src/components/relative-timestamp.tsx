dca898a427b858755bdea25716b7549f409f2ddf
Sam Potter <sam.potter@hotmail.com>
2026-04-29 15:34:39 +1000

    Add todos

--- src/components/relative-timestamp.tsx
+++ src/components/relative-timestamp.tsx
@@ -3,6 +3,9 @@
 import { useEffect, useReducer } from "react";
 import { capitalize, formatRelativeDate } from "@/utils";
 
+// TODO: Use a single interval and update globally
+// TODO: If the delta is greater than a minute then only update every minute
+
 function useRerender(ms: number) {
 	const [, increment] = useReducer((state) => state + 1, 0);
 
