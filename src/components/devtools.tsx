d42353043bf594adbf2b0387b96a33d427a27377
Sam Potter <sam.potter@hotmail.com>
2026-05-07 12:32:03 +1000

    fix(devtools-in-production): Ensure devtools don't reach production (#17)

--- src/components/devtools.tsx
+++ src/components/devtools.tsx
@@ -2,10 +2,12 @@ import { TanStackDevtools } from "@tanstack/react-devtools";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 export function Devtools() {
-	return (
-		<TanStackDevtools
-			config={{ position: "bottom-right" }}
-			plugins={[{ name: "Tanstack Router", render: <TanStackRouterDevtoolsPanel /> }]}
-		/>
-	);
+	if (process.env.NODE_ENV === "development") {
+		return (
+			<TanStackDevtools
+				config={{ position: "bottom-right" }}
+				plugins={[{ name: "Tanstack Router", render: <TanStackRouterDevtoolsPanel /> }]}
+			/>
+		);
+	}
 }
