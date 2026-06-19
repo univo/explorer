e4ba205dbb0daf5848a9d64e682c4e2aa1b1c98a
Sam Potter <sam.potter@hotmail.com>
2026-06-19 15:05:50 +1000

    Add indexes

--- src/events/contract-deployment-v2/event.ts
+++ src/events/contract-deployment-v2/event.ts
@@ -3,7 +3,9 @@ import { getAddress } from "viem";
 import { db } from "@/db/client";
 import { univo } from "@/lib/univo";
 import { tables } from "@/constants";
-import { nonNullable } from "@/utils";
+import { hexToNumber, nonNullable } from "@/utils";
+import { index_account_v2 } from "@/indexes/account-v2";
+import { index_block_number_tx_index_v2 } from "@/indexes/block-number-tx-index-v2";
 import {
 	getDeduplicatedEvents,
 	getEventSuccess,
@@ -59,6 +61,11 @@ export const event = univo.event({
 					success: getEventSuccess(receipt),
 					deployer_address: getAddress(receipt.from),
 					contract_address: getAddress(receipt.contractAddress),
+
+					// Used for indexes
+					chain: hexToNumber(block.eth_chainId),
+					tx_index: hexToNumber(receipt.transactionIndex),
+					block_number: hexToNumber(block.eth_getBlockByNumber.number),
 				};
 			})
 			.filter(nonNullable);
@@ -90,6 +97,33 @@ export const event = univo.event({
 				query: `DELETE FROM event_contract_deployment_v2 WHERE ${v2_getPartitions(batch.map((event) => event.id)).join(" OR ")}`,
 			});
 		},
+	},
+});
+
+univo.event({
+	filters: event.filters,
+	storage: index_block_number_tx_index_v2,
+	id: "contract_deployment_v2_index_block_number_tx_index_v2",
+	handler: (block) => {
+		return event.handler(block).flatMap((event) => {
+			return [
+				{ event_id: event.id, chain: event.chain, block_number: event.block_number, tx_index: event.tx_index }, //
+			];
+		});
+	},
+});
+
+univo.event({
+	filters: event.filters,
+	storage: index_account_v2,
+	id: "contract_deployment_v2_index_account_v2",
+	handler: (block) => {
+		return event.handler(block).flatMap((event) => {
+			return [
+				{ event_id: event.id, account: event.contract_address }, //
+				{ event_id: event.id, account: event.deployer_address }, //
+			];
+		});
 	},
 });
 
