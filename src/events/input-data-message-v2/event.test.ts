43276bcc387f48931f19f240bf1f936fce8bbe43
Sam Potter <sam.potter@hotmail.com>
2026-06-19 10:40:54 +1000

    Add input data message

---
+++ src/events/input-data-message-v2/event.test.ts
@@ -0,0 +1,27 @@
+import { test } from "vitest";
+
+import { getInputDataMessageV2 } from "./event";
+import { test_deleteEvents, test_getBlock, test_v2_getEventIdsForBlock, test_writeEvents } from "@/tests/utils";
+
+test.concurrent("input_data_message_v2", async ({ expect }) => {
+	const block = await test_getBlock({ chain: 1, block_number: 23483288 });
+
+	await test_deleteEvents(block, "event_input_data_message_v2");
+
+	await test_writeEvents(block, "input_data_message_v2");
+
+	const ids = await test_v2_getEventIdsForBlock(block, "event_input_data_message_v2");
+	const events = await getInputDataMessageV2(ids);
+
+	expect(events).toEqual([
+		{
+			from_address: "0x878761636a1dd513463b04a66413c77e8b4eeded",
+			id: "68dd2e5701665398001500000001000c",
+			message:
+				"Blockchain Verified Certificate of Purity for So Pure Supplements XParasite. Batch Code: A5F37E33CB45 Certifying authority: Blockchain Institute of Technology. https://verify.blockchaininstitute.com/ipfs/bafybeigiy6z3mg255bee7wf3pzdekcdqnvj37u55m5agdtrutn7mkuicyq",
+			success: true,
+			tag: "input_data_message_v2",
+			to_address: "0xec84f0b4d6facf98185ba4889ced612e25d02483",
+		},
+	]);
+});
---
+++ src/events/input-data-message-v2/event.ts
@@ -0,0 +1,198 @@
+import { getAddress } from "viem";
+
+import { db } from "@/db/client";
+import { univo } from "@/lib/univo";
+import { tables } from "@/constants";
+import { nonNullable } from "@/utils";
+import {
+	getDeduplicatedEvents,
+	getEventSuccess,
+	v2_createId,
+	v2_getPartition,
+	v2_getPartitions,
+	v2_parseId,
+} from "@/helpers";
+
+export interface InputDataMessageV2 {
+	tag: "input_data_message_v2";
+	id: string;
+	success: boolean;
+	message: string;
+	to_address: `0x${string}`;
+	from_address: `0x${string}`;
+}
+
+// CREATE TABLE event_input_data_message_v2 (
+//     `id` FixedString(16),
+//     `partition` UInt32,
+//     `success` Bool,
+//     `message` String,
+//     `to_address` FixedString(20),
+//     `from_address` FixedString(20)
+// )
+// ENGINE = ReplacingMergeTree
+// ORDER BY id
+// PARTITION BY partition;
+
+univo.event({
+	id: "input_data_message_v2",
+
+	filters: [{ chain: 1, fromBlock: 0 }],
+
+	handler(block) {
+		return block.eth_getBlockByNumber.transactions
+			.map((tx) => {
+				try {
+					if (tx.input === "0x") {
+						return null;
+					}
+
+					if (tx.input === "0x0") {
+						return null;
+					}
+
+					if (tx.input.length < 16) {
+						return null;
+					}
+
+					// When deploying a contract `to` field is null and we know input data is not a message
+					if (tx.to === null) {
+						return null;
+					}
+
+					const message = hex_to_string(tx.input);
+					const num_valid_chars = count_valid_chars(message);
+					const percent_valid_chars = num_valid_chars / message.length;
+
+					// Use some heuristic to decide if input data is not gibberish characters
+					if (percent_valid_chars < 0.6) {
+						return null;
+					}
+
+					const id = v2_createId({
+						logIndex: "0x0",
+						chainId: block.eth_chainId,
+						txIndex: tx.transactionIndex,
+						tableId: tables.input_data_message_v2,
+						blockNumber: block.eth_getBlockByNumber.number,
+						blockTimestamp: block.eth_getBlockByNumber.timestamp,
+					});
+
+					const partition = v2_getPartition(block.eth_getBlockByNumber.timestamp);
+					const receipt = block.eth_getBlockReceipts.find((receipt) => receipt.transactionHash === tx.hash);
+
+					return {
+						id,
+						partition,
+						success: getEventSuccess(receipt),
+						message,
+						to_address: getAddress(tx.to),
+						from_address: getAddress(tx.from),
+					};
+				} catch {
+					return null;
+				}
+			})
+			.filter(nonNullable);
+	},
+
+	storage: {
+		async upsert(batch) {
+			if (batch.length === 0) {
+				return;
+			}
+
+			const values = batch.map((event) => {
+				return `(
+					unhex('${event.id}'),
+					${event.partition},
+					${event.success},
+					${escapeMessageChars(event.message)},
+					unhex('${event.to_address.slice(2)}'),
+					unhex('${event.from_address.slice(2)}')
+				)`;
+			});
+
+			await db.command({
+				query: `INSERT INTO event_input_data_message_v2 (id, partition, success, message, to_address, from_address) VALUES ${values.join(",")}`,
+			});
+		},
+
+		async delete(batch) {
+			await db.command({
+				query: `ALTER TABLE event_input_data_message_v2 UPDATE success = false WHERE ${v2_getPartitions(batch.map((event) => event.id)).join(" OR ")}`,
+			});
+		},
+	},
+});
+
+const decoder = new TextDecoder();
+
+function hex_to_string(hex: `0x${string}`) {
+	const str = hex.slice(2);
+	const bytes = new Uint8Array(str.length / 2);
+
+	for (let i = 0; i < str.length; i += 2) {
+		const byte = str.substring(i, i + 2);
+		bytes[i / 2] = Number.parseInt(byte, 16);
+	}
+
+	return decoder.decode(bytes);
+}
+
+const VALID_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.? ";
+
+function count_valid_chars(string: string) {
+	let count = 0;
+
+	for (const char of string) {
+		if (VALID_CHARS.indexOf(char) >= 0) {
+			count++;
+		}
+	}
+
+	return count;
+}
+
+function escapeMessageChars(value: string) {
+	return `'${value.replaceAll("\\", "\\\\").replaceAll("'", "\\'").replaceAll("\n", "\\n").replaceAll("\r", "\\r").replaceAll("\t", "\\t")}'`;
+}
+
+export async function getInputDataMessageV2(ids: string[]) {
+	const filtered = ids.filter((id) => v2_parseId(id).tableId === tables.input_data_message_v2);
+
+	if (filtered.length === 0) {
+		return [];
+	}
+
+	const partitions = v2_getPartitions(filtered);
+
+	const res = await db.query({
+		query: `
+			SELECT
+				lower(hex(id)),
+				success,
+				message,
+				concat('0x', lower(hex(to_address))) as to_address,
+				concat('0x', lower(hex(from_address))) as from_address
+			FROM event_input_data_message_v2
+			WHERE ${partitions.join(" OR ")};
+		`,
+		format: "JSONEachRow",
+	});
+
+	const rows: any[] = await res.json();
+
+	const events = rows.map<InputDataMessageV2>((row) => {
+		return {
+			tag: "input_data_message_v2",
+			id: row["lower(hex(id))"] as string,
+			success: row.success as boolean,
+			message: row.message as string,
+			to_address: row.to_address as `0x${string}`,
+			from_address: row.from_address as `0x${string}`,
+		};
+	});
+
+	return getDeduplicatedEvents(events);
+}
