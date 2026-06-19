5c578498833676f0b04e0a5f3272b98fc9e3cae9
Sam Potter <sam.potter@hotmail.com>
2026-06-19 11:03:46 +1000

    Update test

--- src/events/native-transfer-v2/event.ts
+++ src/events/native-transfer-v2/event.ts
@@ -137,8 +137,8 @@ export async function getNativeTransferV2(ids: string[]) {
 			id: row["lower(hex(id))"] as string,
 			success: row.success as boolean,
 			quantity: row.quantity as string,
-			to_address: row.to_address as `0x${string}`,
-			from_address: row.from_address as `0x${string}`,
+			to_address: getAddress(row.to_address),
+			from_address: getAddress(row.from_address),
 		};
 	});
 
