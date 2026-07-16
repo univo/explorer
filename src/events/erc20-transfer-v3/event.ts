d7667ce83725bca2b27caf80e29a91b8b79286ae
Sam Potter <sam.potter@hotmail.com>
2026-07-16 10:31:02 +1000

    Add event

---
+++ src/events/erc20-transfer-v3/event.ts
@@ -0,0 +1,87 @@
+import { inArray } from "drizzle-orm";
+import { decodeEventLog, getAddress, parseAbiItem, toEventSelector } from "viem";
+
+import { univo } from "@/lib/univo";
+import { tables } from "@/constants";
+import { schema } from "@/db/schema";
+import { createPostgresClient } from "@/db/client";
+import { nonNullable, numberToHex } from "@/utils";
+import { getEventSuccess, getTxReceiptForLog, createId, getPartition } from "@/helpers";
+
+export interface Erc20TransferV2 {
+	tag: "erc20_transfer_v2";
+	id: string;
+	success: boolean;
+	quantity: `0x${string}`;
+	to_address: `0x${string}`;
+	from_address: `0x${string}`;
+	token_address: `0x${string}`;
+}
+
+const abi = parseAbiItem("event Transfer(address indexed from, address indexed to, uint256 value)");
+
+export const event = univo.event({
+	id: "erc20_transfer_v3",
+
+	filters: [{ chain: 1, fromBlock: 0, event: toEventSelector(abi) }],
+
+	handler: (block) => {
+		return block.eth_getBlockReceipts
+			.flatMap((receipt) => receipt.logs)
+			.filter((log) => log.topics[0] === toEventSelector(abi))
+			.map((log) => {
+				try {
+					const { args } = decodeEventLog({ topics: log.topics, data: log.data, strict: true, abi: [abi] });
+
+					if (args.value === 0n) {
+						return; // Only record non-zero transfers
+					}
+
+					const id = createId({
+						logIndex: log.logIndex,
+						chainId: block.eth_chainId,
+						txIndex: log.transactionIndex,
+						tableId: tables.erc20_transfer_v2,
+						blockNumber: block.eth_getBlockByNumber.number,
+						blockTimestamp: block.eth_getBlockByNumber.timestamp,
+					});
+
+					const partition = getPartition(block.eth_getBlockByNumber.timestamp);
+
+					const receipt = getTxReceiptForLog(block.eth_getBlockReceipts, log);
+
+					return {
+						id,
+						partition,
+						quantity: numberToHex(args.value),
+						success: getEventSuccess(receipt),
+						to_address: getAddress(args.to),
+						from_address: getAddress(args.from),
+						token_address: getAddress(log.address),
+					};
+				} catch (error) {
+					return null;
+				}
+			})
+			.filter(nonNullable);
+	},
+
+	storage: {
+		async upsert(batch) {
+			const client = await createPostgresClient();
+
+			await client.insert(schema.event_erc20_transfer_v3).values(batch);
+		},
+
+		async delete(batch) {
+			const client = await createPostgresClient();
+
+			await client.delete(schema.event_erc20_transfer_v3).where(
+				inArray(
+					schema.event_erc20_transfer_v3.id,
+					batch.map((event) => event.id),
+				),
+			);
+		},
+	},
+});
--- src/routes/api/univo.tsx
+++ src/routes/api/univo.tsx
@@ -5,6 +5,7 @@ import { univo } from "@/lib/univo";
 // We import every event here so that we guarantee all the events will be picked up by the indexer.
 
 import "@/events/erc20-transfer-v2/event";
+import "@/events/erc20-transfer-v3/event";
 import "@/events/erc20-approval-v2/event";
 import "@/events/erc721-approval-v2/event";
 import "@/events/erc721-transfer-v2/event";
