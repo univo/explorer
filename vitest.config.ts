c6da5e41c7e6fa49f178b145d463da8496cd4f98
Sam Potter <sam.potter@hotmail.com>
2026-07-17 09:49:21 +1000

    Fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,34 @@
+import { loadEnv } from "vite";
 import paths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 import { cloudflareTest } from "@cloudflare/vitest-pool-workers";
 
-export default defineConfig({
-	test: {
-		testTimeout: 2 * 60 * 1000,
-		setupFiles: ["./src/tests/setup.ts"],
-	},
-	plugins: [
-		paths({
-			projects: ["./tsconfig.json"],
-		}),
-		cloudflareTest({
-			wrangler: { configPath: "./wrangler.jsonc" },
-		}),
-	],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	const hyperdrive = env.CLOUDFLARE_HYPERDRIVE_LOCAL_CONNECTION_STRING_PG;
+
+	return {
+		test: {
+			testTimeout: 2 * 60 * 1000,
+			setupFiles: ["./src/tests/setup.ts"],
+		},
+
+		plugins: [
+			paths({
+				projects: ["./tsconfig.json"],
+			}),
+			cloudflareTest({
+				miniflare: {
+					hyperdrives: {
+						PG: hyperdrive,
+					},
+				},
+
+				wrangler: {
+					configPath: "./wrangler.jsonc",
+				},
+			}),
+		],
+	};
 });
