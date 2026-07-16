58abcc790ff80891ac98b6e0514e76c9cf7a7220
Sam Potter <sam.potter@hotmail.com>
2026-07-16 14:47:33 +1000

    Increase batch size

--- src/events/erc20-transfer-v3/event.ts
+++ src/events/erc20-transfer-v3/event.ts
@@ -68,7 +68,7 @@ export const event = univo.event({
 
 	storage: {
 		async upsert(batch) {
-			const MAX_BATCH_SIZE = 2000;
+			const MAX_BATCH_SIZE = 4000;
 
 			const client = await createPostgresClient();
 
