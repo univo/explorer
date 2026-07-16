5870972da01c6ffa929d5cb690e9af6e7cced66b
Sam Potter <sam.potter@hotmail.com>
2026-07-16 12:11:33 +1000

    Fix

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,10 +8,39 @@ const id = customType<{ data: string; driverData: Uint8Array | string }>({
 	fromDriver: (value) => bytesToHex(value),
 });
 
-const hex = customType<{ data: `0x${string}`; driverData: Uint8Array | string }>({
-	dataType: () => "bytea",
-	toDriver: (value) => hexToBytes(value),
-	fromDriver: (value) => `0x${bytesToHex(value)}` as const,
+const hex = customType<{ data: `0x${string}` }>({
+	dataType() {
+		return "bytea";
+	},
+	toDriver(value) {
+		let str = value as string;
+
+		if (str.startsWith("0x")) {
+			str = str.slice(2);
+		}
+
+		if (str.length % 2 !== 0) {
+			str = `0${str}`;
+		}
+
+		const pairs = str.match(/[\da-f]{2}/gi);
+
+		if (pairs === null) {
+			throw new Error(`Expected pairs to be defined for value: ${str}`);
+		}
+
+		return new Uint8Array(pairs.map((byte) => Number.parseInt(byte, 16)));
+	},
+	fromDriver(value) {
+		let output = "";
+
+		for (const byte of value as unknown as Uint8Array) {
+			const hex = byte.toString(16);
+			output += hex.length === 1 ? `0${hex}` : hex;
+		}
+
+		return `0x${output}`;
+	},
 });
 
 const event_erc20_transfer_v3 = pgTable("event_erc20_transfer_v3", {
