4035dccbf7329010ab4c30257d94dfd829d1bb41
Sam Potter <sam.potter@hotmail.com>
2026-07-22 14:18:03 +1000

    Update drizzle schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,9 +2,14 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
+	verbose: true,
 	dialect: "postgresql",
 	out: "./src/db/migrations",
 	schema: "./src/db/schema.ts",
+	migrations: {
+		schema: "public",
+		table: "migrations",
+	},
 	dbCredentials: {
 		url: process.env.CLOUDFLARE_HYPERDRIVE_LOCAL_CONNECTION_STRING_PG,
 	},
