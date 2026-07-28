b4570a8d111c41f6d1f67194968b409c6cfd410f
Sam Potter <sam.potter@hotmail.com>
2026-07-29 09:45:39 +1000

    Update constants

--- src/constants.ts
+++ src/constants.ts
@@ -16,8 +16,6 @@ export const CHAINS_REVERSED: Record<number, Chain> = {
 
 // Similarly we use a 2 byte unsigned integer to store table identifiers
 
-// TODO: Move and update to actual table names
-
 export const TABLES = {
 	native_transfer_v1: 0,
 	erc20_transfer_v1: 1,
@@ -28,17 +26,17 @@ export const TABLES = {
 	cancel_pending_tx_v1: 6,
 	erc721_transfer_v1: 7,
 	erc721_approval_v1: 8,
-	erc20_transfer_v2: 9,
-	native_transfer_v2: 10,
-	erc20_approval_v2: 11,
-	input_data_message_v2: 12,
+	erc20_transfer_v3: 9,
+	native_transfer_v3: 10,
+	erc20_approval_v3: 11,
+	input_data_message_v3: 12,
 	contract_deployment_v3: 13,
-	ens_name_registered_v2: 14,
+	ens_name_registered_v3: 14,
 	cancel_pending_tx_v3: 15,
-	erc721_transfer_v2: 16,
-	erc721_approval_v2: 17,
-	tornado_cash_withdrawal_v1: 18,
-	usdc_blacklist_v1: 19,
+	erc721_transfer_v3: 16,
+	erc721_approval_v3: 17,
+	tornado_cash_withdrawal_v3: 18,
+	usdc_blacklist_v3: 19,
 };
 
 // Some of our events operate at the transaction level, i.e. they attempt to classify and interpret a single
