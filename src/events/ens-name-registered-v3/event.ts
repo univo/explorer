1fae1d858612bb91bdb2fdc90514a06d191a46d5
Sam Potter <sam.potter@hotmail.com>
2026-07-29 19:29:02 +1000

    Cs

--- src/events/ens-name-registered-v3/event.ts
+++ src/events/ens-name-registered-v3/event.ts
@@ -3,7 +3,6 @@ import {
 	decodeEventLog,
 	decodeFunctionData,
 	getAddress,
-	isAddressEqual,
 	parseAbiItem,
 	toEventSelector,
 	toFunctionSelector,
@@ -11,11 +10,11 @@ import {
 
 import { table } from "./table";
 import { univo } from "@/lib/univo";
-import { TABLES, TRANSACTION_EVENT } from "@/constants";
-import { nonNullable, numberToHex } from "@/utils";
 import { createPostgresClient } from "@/db/client";
+import { TABLES, TRANSACTION_EVENT } from "@/constants";
 import { index_account_v3 } from "@/indexes/account-v3";
 import { getEventSuccess, createId, parseId } from "@/helpers";
+import { iife, isHexEqual, nonNullable, numberToHex } from "@/utils";
 import { index_block_number_tx_index_v4 } from "@/indexes/block-number-tx-index-v4";
 
 export interface EnsNameRegisteredV3 {
@@ -24,8 +23,8 @@ export interface EnsNameRegisteredV3 {
 	success: boolean;
 	name: string;
 	cost_eth: `0x${string}`;
-	owner_address: `0x${string}`;
 	expires_at: `0x${string}`;
+	owner_address: `0x${string}`;
 }
 
 const V2_DEPLOYED_BLOCK = 9380471;
@@ -48,8 +47,28 @@ export const event = univo.event({
 						return null;
 					}
 
-					const to = tx.to;
-					const registration = getEnsRegistration(to, tx.input);
+					const registration = iife(() => {
+						if (isHexEqual(tx.to!, V2_ADDRESS)) {
+							if (tx.input.startsWith(toFunctionSelector(V2_REGISTER_ABI))) {
+								const { args } = decodeFunctionData({ abi: [V2_REGISTER_ABI], data: tx.input });
+								return { version: "v2" as const, eventAbi: v2, name: args[0], owner: getAddress(args[1]) };
+							}
+
+							if (tx.input.startsWith(toFunctionSelector(V2_REGISTER_WITH_CONFIG_ABI))) {
+								const { args } = decodeFunctionData({ abi: [V2_REGISTER_WITH_CONFIG_ABI], data: tx.input });
+								return { version: "v2" as const, eventAbi: v2, name: args[0], owner: getAddress(args[1]) };
+							}
+						}
+
+						if (isHexEqual(tx.to!, V3_ADDRESS)) {
+							if (tx.input.startsWith(toFunctionSelector(V3_REGISTER_ABI))) {
+								const { args } = decodeFunctionData({ abi: [V3_REGISTER_ABI], data: tx.input });
+								return { version: "v3" as const, eventAbi: v3, name: args[0], owner: getAddress(args[1]) };
+							}
+						}
+
+						return null;
+					});
 
 					if (registration === null) {
 						return null;
@@ -57,12 +76,14 @@ export const event = univo.event({
 
 					const receipt = block.eth_getBlockReceipts.find((receipt) => receipt.transactionHash === tx.hash);
 					const success = getEventSuccess(receipt);
-					let cost = 0n;
-					let expires = 0n;
 
-					if (success) {
+					const { cost, expires } = iife(() => {
+						if (success === false) {
+							return { cost: 0, expires: 0n };
+						}
+
 						const log = receipt?.logs.find(
-							(log) => isAddressEqual(log.address, to) && log.topics[0] === toEventSelector(registration.eventAbi),
+							(log) => isHexEqual(log.address, tx.to!) && log.topics[0] === toEventSelector(registration.eventAbi),
 						);
 
 						if (log === undefined) {
@@ -71,14 +92,12 @@ export const event = univo.event({
 
 						if (registration.version === "v2") {
 							const { args } = decodeEventLog({ data: log.data, topics: log.topics, strict: true, abi: [v2] });
-							cost = args.cost;
-							expires = args.expires;
-						} else {
-							const { args } = decodeEventLog({ data: log.data, topics: log.topics, strict: true, abi: [v3] });
-							cost = args.baseCost + args.premium;
-							expires = args.expires;
+							return { cost: args.cost, expires: args.expires };
 						}
-					}
+
+						const { args } = decodeEventLog({ data: log.data, topics: log.topics, strict: true, abi: [v3] });
+						return { cost: args.baseCost + args.premium, expires: args.expires };
+					});
 
 					const id = createId({
 						logIndex: TRANSACTION_EVENT,
@@ -93,13 +112,13 @@ export const event = univo.event({
 						id,
 						success,
 						name: registration.name,
-						owner_address: registration.owner,
 						cost_eth: numberToHex(cost),
 						expires_at: numberToHex(expires),
+						owner_address: registration.owner,
 
 						// Used for indexes
-						receipt_to: getAddress(to),
-						log_address: getAddress(to),
+						receipt_to: getAddress(tx.to),
+						log_address: getAddress(tx.to),
 						receipt_from: getAddress(tx.from),
 					};
 				} catch {
@@ -122,11 +141,11 @@ export const event = univo.event({
 					.onConflictDoUpdate({
 						target: table.id,
 						set: {
-							success: sql.raw(`excluded.${table.success.name}`),
 							name: sql.raw(`excluded.${table.name.name}`),
+							success: sql.raw(`excluded.${table.success.name}`),
 							cost_eth: sql.raw(`excluded.${table.cost_eth.name}`),
-							owner_address: sql.raw(`excluded.${table.owner_address.name}`),
 							expires_at: sql.raw(`excluded.${table.expires_at.name}`),
+							owner_address: sql.raw(`excluded.${table.owner_address.name}`),
 						},
 					});
 			}
@@ -168,19 +187,47 @@ univo.event({
 	},
 });
 
+export async function getEnsNameRegisteredV3(ids: string[]) {
+	const filtered = ids.filter((id) => parseId(id).tableId === TABLES.ens_name_registered_v3);
+
+	if (filtered.length === 0) {
+		return [];
+	}
+
+	const client = await createPostgresClient();
+
+	const rows = await client
+		.select() //
+		.from(table)
+		.where(inArray(table.id, filtered))
+		.orderBy(asc(table.id));
+
+	return rows.map<EnsNameRegisteredV3>((result) => {
+		return {
+			tag: "ens_name_registered_v3" as const,
+			id: result.id,
+			name: result.name,
+			success: result.success,
+			cost_eth: result.cost_eth,
+			expires_at: result.expires_at,
+			owner_address: getAddress(result.owner_address),
+		};
+	});
+}
+
 const V2_ADDRESS = getAddress("0x283af0b28c62c092c9727f1ee09c02ca627eb7f5");
+
 const V2_REGISTER_ABI = parseAbiItem("function register(string name, address owner, uint256 duration, bytes32 secret)");
+
 const V2_REGISTER_WITH_CONFIG_ABI = parseAbiItem(
 	"function registerWithConfig(string name, address owner, uint256 duration, bytes32 secret, address resolver, address addr)",
 );
-const V2_REGISTER_WITH_CONFIG_SELECTOR = toFunctionSelector(V2_REGISTER_WITH_CONFIG_ABI);
-const V2_REGISTER_SELECTOR = toFunctionSelector(V2_REGISTER_ABI);
 
 const V3_ADDRESS = getAddress("0x253553366da8546fc250f225fe3d25d0c782303b");
+
 const V3_REGISTER_ABI = parseAbiItem(
 	"function register(string name, address owner, uint256 duration, bytes32 secret, address resolver, bytes[] data, bool reverseRecord, uint16 ownerControlledFuses)",
 );
-const V3_REGISTER_SELECTOR = toFunctionSelector(V3_REGISTER_ABI);
 
 const v2 = {
 	type: "event",
@@ -208,52 +255,3 @@ const v3 = {
 		{ indexed: false, internalType: "uint256", name: "expires", type: "uint256" },
 	],
 } as const;
-
-function getEnsRegistration(to: `0x${string}`, input: `0x${string}`) {
-	if (isAddressEqual(to, V2_ADDRESS)) {
-		if (input.startsWith(V2_REGISTER_SELECTOR)) {
-			const { args } = decodeFunctionData({ abi: [V2_REGISTER_ABI], data: input });
-			return { version: "v2" as const, eventAbi: v2, name: args[0], owner: getAddress(args[1]) };
-		}
-
-		if (input.startsWith(V2_REGISTER_WITH_CONFIG_SELECTOR)) {
-			const { args } = decodeFunctionData({ abi: [V2_REGISTER_WITH_CONFIG_ABI], data: input });
-			return { version: "v2" as const, eventAbi: v2, name: args[0], owner: getAddress(args[1]) };
-		}
-	}
-
-	if (isAddressEqual(to, V3_ADDRESS) && input.startsWith(V3_REGISTER_SELECTOR)) {
-		const { args } = decodeFunctionData({ abi: [V3_REGISTER_ABI], data: input });
-		return { version: "v3" as const, eventAbi: v3, name: args[0], owner: getAddress(args[1]) };
-	}
-
-	return null;
-}
-
-export async function getEnsNameRegisteredV3(ids: string[]) {
-	const filtered = ids.filter((id) => parseId(id).tableId === TABLES.ens_name_registered_v3);
-
-	if (filtered.length === 0) {
-		return [];
-	}
-
-	const client = await createPostgresClient();
-
-	const rows = await client
-		.select() //
-		.from(table)
-		.where(inArray(table.id, filtered))
-		.orderBy(asc(table.id));
-
-	return rows.map<EnsNameRegisteredV3>((result) => {
-		return {
-			tag: "ens_name_registered_v3" as const,
-			id: result.id,
-			success: result.success,
-			name: result.name,
-			cost_eth: result.cost_eth,
-			owner_address: getAddress(result.owner_address),
-			expires_at: result.expires_at,
-		};
-	});
-}
