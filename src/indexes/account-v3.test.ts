ca121b267eae36908d74b289017850fbf78c5de1
Sam Potter <sam.potter@hotmail.com>
2026-07-29 10:54:08 +1000

    Update test

--- src/indexes/account-v3.test.ts
+++ src/indexes/account-v3.test.ts
@@ -1,7 +1,7 @@
 import { test } from "vitest";
 
 import { test_getBlock } from "@/tests/utils";
-import { event } from "@/events/erc20-transfer-v3/event";
+import { event } from "@/events/native-transfer-v3/event";
 import { getEventIdsForAccount, index_account_v3 } from "./account-v3";
 
 test.concurrent("account-v3", async ({ expect }) => {
@@ -13,7 +13,6 @@ test.concurrent("account-v3", async ({ expect }) => {
 		return [
 			{ event_id: event.id, account: event.to_address }, //
 			{ event_id: event.id, account: event.from_address },
-			{ event_id: event.id, account: event.token_address },
 		];
 	});
 
@@ -21,27 +20,14 @@ test.concurrent("account-v3", async ({ expect }) => {
 
 	await index_account_v3.upsert(indexes);
 
-	const ids = await getEventIdsForAccount("0xdAC17F958D2ee523a2206206994597C13D831ec7", {
+	const ids = await getEventIdsForAccount("0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8", {
 		limit: 14,
 		order: "reverse",
 	});
 
 	expect(ids).toMatchInlineSnapshot(`
 		[
-		  "5eb0170500989680000700000300010009",
-		  "5eb0170500989680001100000800010009",
-		  "5eb0170500989680003900005800010009",
-		  "5eb0170500989680003c00005b00010009",
-		  "5eb0170500989680003d00005c00010009",
-		  "5eb0170500989680003f00006100010009",
-		  "5eb0170500989680004000006200010009",
-		  "5eb0170500989680004400006700010009",
-		  "5eb0170500989680004500006800010009",
-		  "5eb0170500989680004b00006c00010009",
-		  "5eb0170500989680005100007700010009",
-		  "5eb0170500989680005800007e00010009",
-		  "5eb0170500989680005e00008200010009",
-		  "5eb0170500989680006300008600010009",
+		  "5eb01705009896800000ffffff0001000a",
 		]
 	`);
 });
