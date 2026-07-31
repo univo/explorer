f1d36a3860ef228ef2cd0e61cea99bca13db465d
Sam Potter <sam.potter@hotmail.com>
2026-07-31 15:24:46 +1000

    Cs

--- src/routes/api/univo.tsx
+++ src/routes/api/univo.tsx
@@ -6,17 +6,17 @@ import { univo } from "@/lib/univo";
 
 import "@/events/intent_fwa_won_v1/event";
 import "@/events/usdc-blacklist-v3/event";
-import "@/events/log_fwa_nft_listed_v1/event";
 import "@/events/erc20-transfer-v3/event";
 import "@/events/erc20-approval-v3/event";
 import "@/events/erc721-approval-v3/event";
 import "@/events/erc721-transfer-v3/event";
 import "@/events/native-transfer-v3/event";
-import "@/events/intent_fwa_deposited_v1/event";
 import "@/events/cancel-pending-tx-v3/event";
+import "@/events/log_fwa_nft_listed_v1/event";
 import "@/events/input-data-message-v3/event";
 import "@/events/contract-deployment-v3/event";
 import "@/events/ens-name-registered-v3/event";
+import "@/events/intent_fwa_deposited_v1/event";
 import "@/events/tornado-cash-withdrawal-v3/event";
 
 export const Route = createFileRoute("/api/univo")({
