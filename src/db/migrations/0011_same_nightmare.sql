501ef499e3e70f3cce393af07b2faf55bcf9af9f
Sam Potter <sam.potter@hotmail.com>
2026-08-10 11:48:26 +1000

    Delete migrated events

---
+++ src/db/migrations/0011_same_nightmare.sql
@@ -0,0 +1,11 @@
+DROP TABLE "event_cancel_pending_tx_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_contract_deployment_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_ens_name_registered_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_erc20_approval_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_erc20_transfer_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_erc721_approval_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_erc721_transfer_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_input_data_message_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_native_transfer_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_tornado_cash_withdrawal_v3" CASCADE;--> statement-breakpoint
+DROP TABLE "event_usdc_blacklist_v3" CASCADE;
--- src/events/usdc-blacklist-v3/table.ts
+++
@@ -1,9 +0,0 @@
-import { boolean, pgTable } from "drizzle-orm/pg-core";
-
-import { hex, id } from "@/db/types";
-
-export const table = pgTable("event_usdc_blacklist_v3", {
-	id: id().primaryKey(),
-	success: boolean().notNull(),
-	account_address: hex().notNull(),
-});
