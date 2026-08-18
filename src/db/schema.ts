d5aa564e3c0debf1cbc0df2556a822745a1a6c6f
Sam Potter <sam.potter@hotmail.com>
2026-08-18 19:09:27 +1000

    Cs

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,8 +9,8 @@ export { table as log_fwa_nft_listed_v1 } from "@/events/log_fwa_nft_listed_v1/t
 export { table as log_erc721_transfer_v1 } from "@/events/log_erc721_transfer_v1/table";
 export { table as log_erc721_approval_v1 } from "@/events/log_erc721_approval_v1/table";
 export { table as log_uniswap_v3_swap_v1 } from "@/events/log_uniswap_v3_swap_v1/table";
-export { table as log_uniswap_v3_pool_created_v1 } from "@/events/log_uniswap_v3_pool_created_v1/table";
 export { table as log_ens_reverse_claimed_v1 } from "@/events/log_ens_reverse_claimed_v1/table";
+export { table as log_uniswap_v3_pool_created_v1 } from "@/events/log_uniswap_v3_pool_created_v1/table";
 export { table as log_ens_name_for_addr_changed_v1 } from "@/events/log_ens_name_for_addr_changed_v1/table";
 
 export { table as intent_idm_v1 } from "@/events/intent_idm_v1/table";
--- src/routes/api/univo.tsx
+++ src/routes/api/univo.tsx
@@ -12,8 +12,6 @@ import "@/events/log_fwa_nft_listed_v1/event";
 import "@/events/log_erc721_transfer_v1/event";
 import "@/events/log_erc721_approval_v1/event";
 import "@/events/log_uniswap_v3_swap_v1/event";
-import "@/events/log_ens_reverse_claimed_v1/event";
-import "@/events/log_ens_name_for_addr_changed_v1/event";
 import "@/events/intent_fwa_deposited_v1/event";
 import "@/events/intent_aave_v3_repay_v1/event";
 import "@/events/intent_usdc_blacklist_v1/event";
@@ -23,11 +21,13 @@ import "@/events/intent_native_transfer_v1/event";
 import "@/events/intent_uniswap_v3_swap_v1/event";
 import "@/events/intent_uniswap_v3_mint_v1/event";
 import "@/events/intent_aave_v3_withdraw_v1/event";
+import "@/events/log_ens_reverse_claimed_v1/event";
 import "@/events/intent_cancel_pending_tx_v1/event";
 import "@/events/intent_tornado_withdrawal_v1/event";
 import "@/events/intent_contract_deployment_v1/event";
 import "@/events/intent_ens_name_registered_v1/event";
 import "@/events/log_uniswap_v3_pool_created_v1/event";
+import "@/events/log_ens_name_for_addr_changed_v1/event";
 
 export const Route = createFileRoute("/api/univo")({
 	server: {
