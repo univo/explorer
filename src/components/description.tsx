0e146c3dd218dc9d6fb40ad6a5dd9b30320124a1
Sam Potter <sam.potter@hotmail.com>
2026-08-25 19:18:18 +1000

    Add todo

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from "react";
 
+// TODO: Add `success` as a prop that includes the exclamation icon
+
 export function Description(props: { children: ReactNode }) {
 	return <div className="flex items-center flex-wrap wrap-anywhere text-sm text-gray-900 gap-1">{props.children}</div>;
 }
