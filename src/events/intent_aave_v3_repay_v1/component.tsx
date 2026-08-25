41fb43b64d66cf9d176293faac0bcace049f6ba5
Sam Potter <sam.potter@hotmail.com>
2026-08-26 09:21:07 +1000

    Copy

--- src/events/intent_aave_v3_repay_v1/component.tsx
+++ src/events/intent_aave_v3_repay_v1/component.tsx
@@ -1,84 +1,20 @@
 import { maxUint256 } from "viem";
 
 import { parseId } from "@/helpers";
-import { isHexEqual } from "@/utils";
 import { Erc20 } from "@/components/erc-20";
 import { Action } from "@/components/action";
 import { Account } from "@/components/account";
+import { isHexEqual, unreachable } from "@/utils";
 import { ExclamationIcon } from "@/components/icons";
 import { Description } from "@/components/description";
 import { AAVE_V3_ETHEREUM_POOL_ADDRESS, type IntentAaveV3RepayV1 } from "./event";
-
-export function IntentAaveV3RepayV1Description(props: { event: IntentAaveV3RepayV1 }) {
-	const { chainId: chain, blockTimestamp } = parseId(props.event.id);
-	const quantity = BigInt(props.event.quantity);
-	const all = quantity === maxUint256;
-
-	if (isHexEqual(props.event.repayer_address, props.event.on_behalf_of_address)) {
-		if (all) {
-			return (
-				<Description>
-					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
-					<Account chain={chain} address={props.event.repayer_address} />
-					<Action type="repaid">repaid</Action>
-					<span>their entire</span>
-					<Erc20 chain={chain} address={props.event.token_address} at={blockTimestamp} />
-					<span>debt obligation to</span>
-					<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
-				</Description>
-			);
-		}
-
-		return (
-			<Description>
-				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
-				<Account chain={chain} address={props.event.repayer_address} />
-				<span>partially</span>
-				<Action type="repaid">repaid</Action>
-				<Erc20 chain={chain} address={props.event.token_address} quantity={quantity} at={blockTimestamp} />
-				<span>of their debt obligation to</span>
-				<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
-			</Description>
-		);
-	}
-
-	if (all) {
-		return (
-			<Description>
-				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
-				<Account chain={chain} address={props.event.repayer_address} />
-				<Action type="repaid">repaid</Action>
-				<span>the entire</span>
-				<Erc20 chain={chain} address={props.event.token_address} at={blockTimestamp} />
-				<span>debt obligation owned by</span>
-				<Account chain={chain} address={props.event.on_behalf_of_address} />
-				<span>to</span>
-				<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
-			</Description>
-		);
-	}
-
-	return (
-		<Description>
-			{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
-			<Account chain={chain} address={props.event.repayer_address} />
-			<span>partially</span>
-			<Action type="repaid">repaid</Action>
-			<Erc20 chain={chain} address={props.event.token_address} quantity={quantity} at={blockTimestamp} />
-			<span>of debt owned by</span>
-			<Account chain={chain} address={props.event.on_behalf_of_address} />
-			<span>to</span>
-			<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
-		</Description>
-	);
-}
 
 export function IntentAaveV3RepayV1AccountDescription(props: { event: IntentAaveV3RepayV1; address: `0x${string}` }) {
 	const { chainId: chain, blockTimestamp } = parseId(props.event.id);
 	const quantity = BigInt(props.event.quantity);
 	const all = quantity === maxUint256;
 
-	// 1. From the perspective of the repayer
+	// (tx.from) repayer_address: repaying the debt
 
 	if (isHexEqual(props.address, props.event.repayer_address)) {
 		if (isHexEqual(props.event.repayer_address, props.event.on_behalf_of_address)) {
@@ -87,9 +23,9 @@ export function IntentAaveV3RepayV1AccountDescription(props: { event: IntentAave
 					<Description>
 						{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
 						<Action type="repaid">Repay</Action>
-						<span>their entire</span>
+						<span>the entire</span>
 						<Erc20 chain={chain} address={props.event.token_address} at={blockTimestamp} />
-						<span>debt obligation to</span>
+						<span>debt with</span>
 						<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
 					</Description>
 				);
@@ -99,9 +35,9 @@ export function IntentAaveV3RepayV1AccountDescription(props: { event: IntentAave
 				<Description>
 					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
 					<span>Partially</span>
-					<Action type="repaid">Repay</Action>
+					<Action type="repaid">repay</Action>
 					<Erc20 chain={chain} address={props.event.token_address} quantity={quantity} at={blockTimestamp} />
-					<span>of their debt obligation to</span>
+					<span>of debt with</span>
 					<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
 				</Description>
 			);
@@ -114,9 +50,9 @@ export function IntentAaveV3RepayV1AccountDescription(props: { event: IntentAave
 					<Action type="repaid">Repay</Action>
 					<span>the entire</span>
 					<Erc20 chain={chain} address={props.event.token_address} at={blockTimestamp} />
-					<span>debt obligation owned by</span>
+					<span>debt to</span>
 					<Account chain={chain} address={props.event.on_behalf_of_address} />
-					<span>to</span>
+					<span>with</span>
 					<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
 				</Description>
 			);
@@ -126,15 +62,164 @@ export function IntentAaveV3RepayV1AccountDescription(props: { event: IntentAave
 			<Description>
 				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
 				<span>Partially</span>
-				<Action type="repaid">Repay</Action>
+				<Action type="repaid">repay</Action>
 				<Erc20 chain={chain} address={props.event.token_address} quantity={quantity} at={blockTimestamp} />
-				<span>of debt owned by</span>
+				<span>of debt to</span>
 				<Account chain={chain} address={props.event.on_behalf_of_address} />
-				<span>to</span>
+				<span>with</span>
 				<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
 			</Description>
 		);
 	}
 
-	return <IntentAaveV3RepayV1Description event={props.event} />;
+	// on_behalf_of_address: when repaying the debt owned by a different account. To get here means we failed the
+	// previous check and this is not the repaying address but simply the beneficiary of the repayment
+
+	if (isHexEqual(props.address, props.event.on_behalf_of_address)) {
+		if (all) {
+			return (
+				<Description>
+					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+					<Account chain={chain} address={props.event.repayer_address} />
+					<Action type="repaid">repaid</Action>
+					<span>our entire</span>
+					<Erc20 chain={chain} address={props.event.token_address} at={blockTimestamp} />
+					<span>debt with</span>
+					<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
+				</Description>
+			);
+		}
+
+		return (
+			<Description>
+				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+				<Account chain={chain} address={props.event.repayer_address} />
+				<span>partially</span>
+				<Action type="repaid">repay</Action>
+				<Erc20 chain={chain} address={props.event.token_address} quantity={props.event.quantity} at={blockTimestamp} />
+				<span>of our debt with</span>
+				<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
+			</Description>
+		);
+	}
+
+	// (tx.to) AAVE_V3_ETHEREUM_POOL_ADDRESS: the contract facilitating the repayment
+
+	if (isHexEqual(props.address, AAVE_V3_ETHEREUM_POOL_ADDRESS)) {
+		if (isHexEqual(props.event.repayer_address, props.event.on_behalf_of_address)) {
+			if (all) {
+				return (
+					<Description>
+						{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+						<Account chain={chain} address={props.event.repayer_address} />
+						<Action type="repaid">repays</Action>
+						<span>their entire</span>
+						<Erc20 chain={chain} address={props.event.token_address} at={blockTimestamp} />
+						<span>debt</span>
+					</Description>
+				);
+			}
+
+			return (
+				<Description>
+					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+					<Account chain={chain} address={props.event.repayer_address} />
+					<span>partially</span>
+					<Action type="repaid">repays</Action>
+					<Erc20 chain={chain} address={props.event.token_address} quantity={quantity} at={blockTimestamp} />
+					<span>of their debt</span>
+				</Description>
+			);
+		}
+
+		if (all) {
+			return (
+				<Description>
+					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+					<Account chain={chain} address={props.event.repayer_address} />
+					<Action type="repaid">repays</Action>
+					<span>the entire</span>
+					<Erc20 chain={chain} address={props.event.token_address} at={blockTimestamp} />
+					<span>debt to</span>
+					<Account chain={chain} address={props.event.on_behalf_of_address} />
+				</Description>
+			);
+		}
+
+		return (
+			<Description>
+				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+				<Account chain={chain} address={props.event.repayer_address} />
+				<span>partially</span>
+				<Action type="repaid">repays</Action>
+				<Erc20 chain={chain} address={props.event.token_address} quantity={quantity} at={blockTimestamp} />
+				<span>of debt to</span>
+				<Account chain={chain} address={props.event.on_behalf_of_address} />
+			</Description>
+		);
+	}
+
+	// token_address: the asset repaid
+
+	if (isHexEqual(props.address, props.event.token_address)) {
+		if (isHexEqual(props.event.repayer_address, props.event.on_behalf_of_address)) {
+			if (all) {
+				return (
+					<Description>
+						{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+						<Account chain={chain} address={props.event.repayer_address} />
+						<Action type="repaid">repays</Action>
+						<span>their entire</span>
+						<Erc20 chain={chain} address={props.event.token_address} at={blockTimestamp} />
+						<span>debt with</span>
+						<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
+					</Description>
+				);
+			}
+
+			return (
+				<Description>
+					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+					<Account chain={chain} address={props.event.repayer_address} />
+					<span>partially</span>
+					<Action type="repaid">repays</Action>
+					<Erc20 chain={chain} address={props.event.token_address} quantity={quantity} at={blockTimestamp} />
+					<span>of their debt with</span>
+					<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
+				</Description>
+			);
+		}
+
+		if (all) {
+			return (
+				<Description>
+					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+					<Account chain={chain} address={props.event.repayer_address} />
+					<Action type="repaid">repays</Action>
+					<span>the entire</span>
+					<Erc20 chain={chain} address={props.event.token_address} at={blockTimestamp} />
+					<span>debt to</span>
+					<Account chain={chain} address={props.event.on_behalf_of_address} />
+					<span>with</span>
+					<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
+				</Description>
+			);
+		}
+
+		return (
+			<Description>
+				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+				<Account chain={chain} address={props.event.repayer_address} />
+				<span>partially</span>
+				<Action type="repaid">repays</Action>
+				<Erc20 chain={chain} address={props.event.token_address} quantity={quantity} at={blockTimestamp} />
+				<span>of debt to</span>
+				<Account chain={chain} address={props.event.on_behalf_of_address} />
+				<span>with</span>
+				<Account chain={chain} address={AAVE_V3_ETHEREUM_POOL_ADDRESS} />
+			</Description>
+		);
+	}
+
+	unreachable();
 }
