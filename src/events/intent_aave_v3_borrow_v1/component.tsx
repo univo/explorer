f8eaafad7246882793a672c94879a2dbf3a360d4
Sam Potter <sam.potter@hotmail.com>
2026-08-25 12:44:33 +1000

    Comments

--- src/events/intent_aave_v3_borrow_v1/component.tsx
+++ src/events/intent_aave_v3_borrow_v1/component.tsx
@@ -7,18 +7,11 @@ import { ExclamationIcon } from "@/components/icons";
 import { Description } from "@/components/description";
 import { AAVE_V3_ETHEREUM_POOL_ADDRESS, type IntentAaveV3BorrowV1 } from "./event";
 
-// Do we change up the columns for the block view to include `actor`?
 // The end goal would be nice if intents = account views and logs = objective views
 // To get there, we need to solve the issue with fallbacks and the block view
-// We can solve the first by ensuring we define an account view for all accounts registered in the index (ensures tx.from is covered)
-// Then we can add an actor field to the block view
 
-// FINALISED THINKING
-
-// 1. We never show log events from the account perspective TRUE
-// 2. Intents should not have an objective view, rn they are needed as fallback and for the block view PENDING
-// 3. We don't need to provide a fallback to the account description if it covers all accounts specified in the index PENDING
-// 4. We must always specify an account view for tx.from (or whatever field it's stored as) so we have something to show in the tx action
+// Can solve fallbacks by define an account view for all accounts registered in the index (ensures tx.from is covered)
+// Either remove the block view, or only show log events?
 
 export function IntentAaveV3BorrowV1Description(props: { event: IntentAaveV3BorrowV1 }) {
 	const { chainId: chain, blockTimestamp } = parseId(props.event.id);
