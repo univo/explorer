8643c2c7033b6d4dea5b915c9303175e59a23d7a
Sam Potter <sam.potter@hotmail.com>
2026-08-25 20:12:19 +1000

    Copy

--- src/events/intent_cancel_pending_tx_v1/component.tsx
+++ src/events/intent_cancel_pending_tx_v1/component.tsx
@@ -1,32 +1,11 @@
-import { isAddressEqual } from "viem";
-
-import { parseId } from "@/helpers";
-import { formatNumber } from "@/utils";
 import { Action } from "@/components/action";
-import { Account } from "@/components/account";
 import { ExclamationIcon } from "@/components/icons";
 import { Description } from "@/components/description";
 import type { IntentCancelPendingTxV1 } from "./event";
+import { formatNumber, isHexEqual, unreachable } from "@/utils";
 
-export function IntentCancelPendingTxV1Description(props: { event: IntentCancelPendingTxV1 }) {
-	const chain = parseId(props.event.id).chainId;
-
-	return (
-		<Description>
-			{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
-			<Account chain={chain} address={props.event.from_address} />
-			<Action type="cancelled">cancelled</Action>
-			<span>pending transaction with nonce</span>
-			<span>{formatNumber(BigInt(props.event.nonce))}</span>
-		</Description>
-	);
-}
-
-export function IntentCancelPendingTxV1AccountDescription(props: {
-	event: IntentCancelPendingTxV1;
-	address: `0x${string}`;
-}) {
-	if (isAddressEqual(props.address, props.event.from_address)) {
+export function IntentCancelPendingTxV1AccountDescription(props: { event: IntentCancelPendingTxV1; address: `0x${string}` }) {
+	if (isHexEqual(props.address, props.event.from_address)) {
 		return (
 			<Description>
 				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
@@ -37,5 +16,5 @@ export function IntentCancelPendingTxV1AccountDescription(props: {
 		);
 	}
 
-	return <IntentCancelPendingTxV1Description event={props.event} />;
+	unreachable();
 }
