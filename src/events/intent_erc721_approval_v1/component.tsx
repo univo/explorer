a6c0f397f67b43bcc6558ccccd4e0e99d4e22e71
Sam Potter <sam.potter@hotmail.com>
2026-08-26 11:32:20 +1000

    Copy

--- src/events/intent_erc721_approval_v1/component.tsx
+++ src/events/intent_erc721_approval_v1/component.tsx
@@ -1,57 +1,29 @@
-import { isAddressEqual } from "viem";
-
 import { parseId } from "@/helpers";
 import { ZERO_ADDRESS } from "@/constants";
 import { Action } from "@/components/action";
 import { Erc721 } from "@/components/erc-721";
 import { Account } from "@/components/account";
+import { isHexEqual, unreachable } from "@/utils";
 import { ExclamationIcon } from "@/components/icons";
 import type { IntentErc721ApprovalV1 } from "./event";
 import { Description } from "@/components/description";
 
-export function IntentErc721ApprovalV1Description(props: { event: IntentErc721ApprovalV1 }) {
+export function IntentErc721ApprovalV1AccountDescription(props: { event: IntentErc721ApprovalV1; address: `0x${string}` }) {
 	const chain = parseId(props.event.id).chainId;
+	const revoked = isHexEqual(props.event.spender_address, ZERO_ADDRESS);
 
-	if (isAddressEqual(props.event.spender_address, ZERO_ADDRESS)) {
-		return (
-			<Description>
-				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
-				<Account chain={chain} address={props.event.caller_address} />
-				<Action type="revoked">revoked</Action>
-				<span>approval for</span>
-				<Erc721 chain={chain} address={props.event.token_address} id={props.event.token_id} />
-				<span>to be transferred</span>
-			</Description>
-		);
-	}
+	// Note that for all the `revoked` code paths the spender is the null address
 
-	return (
-		<Description>
-			{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
-			<Account chain={chain} address={props.event.caller_address} />
-			<Action type="approved">approved</Action>
-			<Account chain={chain} address={props.event.spender_address} />
-			<span>to transfer</span>
-			<Erc721 chain={chain} address={props.event.token_address} id={props.event.token_id} />
-		</Description>
-	);
-}
+	// (tx.from) caller_address
 
-export function IntentErc721ApprovalV1AccountDescription(props: {
-	event: IntentErc721ApprovalV1;
-	address: `0x${string}`;
-}) {
-	const chain = parseId(props.event.id).chainId;
-
-	if (isAddressEqual(props.address, props.event.caller_address)) {
-		if (isAddressEqual(props.event.spender_address, ZERO_ADDRESS)) {
+	if (isHexEqual(props.address, props.event.caller_address)) {
+		if (revoked) {
 			return (
 				<Description>
 					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
 					<Action type="revoked">Revoke</Action>
-					<span>approval for</span>
+					<span>approval to transfer</span>
 					<Erc721 chain={chain} address={props.event.token_address} id={props.event.token_id} />
-					<span>to be transferred</span>
 				</Description>
 			);
 		}
@@ -67,5 +39,58 @@ export function IntentErc721ApprovalV1AccountDescription(props: {
 		);
 	}
 
-	return <IntentErc721ApprovalV1Description event={props.event} />;
+	// (tx.to) token_address
+
+	if (isHexEqual(props.address, props.event.token_address)) {
+		if (revoked) {
+			return (
+				<Description>
+					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+					<Account chain={chain} address={props.event.caller_address} />
+					<Action type="revoked">revokes</Action>
+					<span>approval to transfer</span>
+					<Erc721 chain={chain} address={props.event.token_address} id={props.event.token_id} />
+				</Description>
+			);
+		}
+
+		return (
+			<Description>
+				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+				<Account chain={chain} address={props.event.caller_address} />
+				<Action type="approved">approves</Action>
+				<Account chain={chain} address={props.event.spender_address} />
+				<span>to transfer</span>
+				<Erc721 chain={chain} address={props.event.token_address} id={props.event.token_id} />
+			</Description>
+		);
+	}
+
+	// spender_address
+
+	if (isHexEqual(props.address, props.event.spender_address)) {
+		if (revoked) {
+			return (
+				<Description>
+					{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+					<Account chain={chain} address={props.event.caller_address} />
+					<Action type="revoked">revokes</Action>
+					<span>approval to transfer</span>
+					<Erc721 chain={chain} address={props.event.token_address} id={props.event.token_id} />
+				</Description>
+			);
+		}
+
+		return (
+			<Description>
+				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+				<Account chain={chain} address={props.event.caller_address} />
+				<Action type="approved">approves</Action>
+				<span>this account to transfer</span>
+				<Erc721 chain={chain} address={props.event.token_address} id={props.event.token_id} />
+			</Description>
+		);
+	}
+
+	unreachable();
 }
