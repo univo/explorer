9919cbcced2034171b7de8caa27451012e8a3ff8
Sam Potter <sam.potter@hotmail.com>
2026-08-26 10:57:00 +1000

    Copy

--- src/events/intent_erc20_transfer_v1/component.tsx
+++ src/events/intent_erc20_transfer_v1/component.tsx
@@ -1,35 +1,18 @@
-import { isAddressEqual } from "viem";
-
 import { parseId } from "@/helpers";
 import { Erc20 } from "@/components/erc-20";
 import { Action } from "@/components/action";
 import { Account } from "@/components/account";
+import { unreachable, isHexEqual } from "@/utils";
 import type { IntentErc20TransferV1 } from "./event";
 import { ExclamationIcon } from "@/components/icons";
 import { Description } from "@/components/description";
 
-export function IntentErc20TransferV1Description(props: { event: IntentErc20TransferV1 }) {
+export function IntentErc20TransferV1AccountDescription(props: { event: IntentErc20TransferV1; address: `0x${string}` }) {
 	const { chainId: chain, blockTimestamp } = parseId(props.event.id);
 
-	return (
-		<Description>
-			{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
-			<Account chain={chain} address={props.event.from_address} />
-			<Action type="sent">sent</Action>
-			<Erc20 chain={chain} address={props.event.token_address} quantity={props.event.quantity} at={blockTimestamp} />
-			<span>to</span>
-			<Account chain={chain} address={props.event.to_address} />
-		</Description>
-	);
-}
+	// (tx.from) from_address
 
-export function IntentErc20TransferV1AccountDescription(props: {
-	event: IntentErc20TransferV1;
-	address: `0x${string}`;
-}) {
-	const { chainId: chain, blockTimestamp } = parseId(props.event.id);
-
-	if (isAddressEqual(props.address, props.event.from_address)) {
+	if (isHexEqual(props.address, props.event.from_address)) {
 		return (
 			<Description>
 				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
@@ -41,7 +24,9 @@ export function IntentErc20TransferV1AccountDescription(props: {
 		);
 	}
 
-	if (isAddressEqual(props.address, props.event.to_address)) {
+	// (tx.to) to_address
+
+	if (isHexEqual(props.address, props.event.to_address)) {
 		return (
 			<Description>
 				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
@@ -53,5 +38,20 @@ export function IntentErc20TransferV1AccountDescription(props: {
 		);
 	}
 
-	return <IntentErc20TransferV1Description event={props.event} />;
+	// token_address
+
+	if (isHexEqual(props.address, props.event.token_address)) {
+		return (
+			<Description>
+				{props.event.success === false && <ExclamationIcon className="size-4 text-red-500" />}
+				<Account chain={chain} address={props.event.from_address} />
+				<Action type="sent">sends</Action>
+				<Erc20 chain={chain} address={props.event.token_address} quantity={props.event.quantity} at={blockTimestamp} />
+				<span>to</span>
+				<Account chain={chain} address={props.event.to_address} />
+			</Description>
+		);
+	}
+
+	unreachable();
 }
