9029fc6476e5481eec05e9fe62a2f6c5047f83a9
Sam Potter <sam.potter@hotmail.com>
2026-09-01 10:06:12 +1000

    Fix test

--- src/events/log_fwa_nft_allocated_v1/event.test.ts
+++ src/events/log_fwa_nft_allocated_v1/event.test.ts
@@ -37,7 +37,7 @@ test.concurrent("log_fwa_nft_allocated_v1 deletes, writes, and reads from storag
 		    "backing_eth": "0x9536c708910000",
 		    "depositor_address": "0x03594D72e895e4ecD716e472C84815233047e566",
 		    "id": "6a6abceb0187473900df0004d100010032",
-		    "listing_id": "0x16f9b",
+		    "listing_id": "0x016f9b",
 		    "purchaser_address": "0xa89C876BE69223295A0925D7A62Cb6868dEc4ac8",
 		    "success": true,
 		    "tag": "log_fwa_nft_allocated_v1",
