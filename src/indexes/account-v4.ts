d9517a7f39958f0a3ce22f86a47c4ad9ecc22ec1
Sam Potter <sam.potter@hotmail.com>
2026-09-08 14:42:33 +1000

    Implement

--- src/indexes/account-v4.ts
+++ src/indexes/account-v4.ts
@@ -1,16 +1,21 @@
 import { getAddress } from "viem";
-import { and, asc, desc, eq, gt, lt } from "drizzle-orm";
+import { and, asc, desc, eq, gt, inArray, lt } from "drizzle-orm";
 import { index, integer, pgTable, smallint } from "drizzle-orm/pg-core";
 
-import { logger } from "@/utils";
 import { inTuple, hex } from "@/db/types";
+import { logger, numberToHex } from "@/utils";
+import { createId, parseId } from "@/helpers";
 import { createPostgresClient } from "@/db/client";
+import { TABLES, type Chain, type Table } from "@/constants";
 
 // This table uses indexes slightly differently than others. Noticably, we use a normal index over both columns as opposed
 // to a primary key. This means duplicates are possible and we do not enforce uniqueness. Note the usage of selectDistinct
-// in our query to compensate for those duplicates. We do this to reduce the index size. After partitioning by timestamp,
-// we reduce the search space so significantly that the any values after that in the index aren't worth including. This
-// increases CPU and memory caused by the scan but is a good tradeoff.
+// in our query to compensate for those duplicates.
+
+// There are two reasons why we do this: it trades off a reduced storage costs for a increased CPU costs. After partitioning
+// by timestamp we reduce the search space so significantly that we can just perform a scan over the remaining values. The
+// second reason and slightly more important is that an index can be added after the first backfill. When initialising the
+// explorer this massively improves insert performance.
 
 type Index = {
 	account: `0x${string}`;
@@ -47,7 +52,7 @@ export const index_account_v4 = {
 	async upsert(indexes: Index[]) {
 		const unique: Record<string, true> = {};
 
-		const batch: Index[] = [];
+		const batch: (typeof table.$inferInsert)[] = [];
 
 		for (const index of indexes) {
 			const account = getAddress(index.account);
@@ -60,7 +65,18 @@ export const index_account_v4 = {
 
 			unique[key] = true;
 
-			batch.push({ account, event_id: index.event_id });
+			const parsed = parseId(index.event_id);
+
+			batch.push({
+				account,
+				chain: parsed.chainId,
+				table_id: parsed.tableId,
+				block_timestamp: parsed.blockTimestamp,
+
+				tx_index: parsed.txIndex,
+				log_index: parsed.logIndex,
+				block_number: parsed.blockNumber,
+			});
 		}
 
 		const MAX_BATCH_SIZE = 8000;
@@ -77,60 +93,131 @@ export const index_account_v4 = {
 
 		await client.delete(table).where(
 			inTuple(
-				[table.account, table.event_id],
-				indexes.map((index) => [index.account, index.event_id]),
+				[table.account, table.chain, table.table_id, table.block_timestamp],
+				indexes.map((index) => {
+					const parsed = parseId(index.event_id);
+
+					return [index.account, parsed.chainId, parsed.tableId, parsed.blockTimestamp];
+				}),
 			),
 		);
 	},
 };
 
-// The index aims to support a few primary query patterns:
-//
-// 1. Filter events by account, event, and timestamp: if we are looking for an account performing
-//    specific events we can filter based on the table id, and provide a timestamp to search within
-//    a given time range.
-//
-// 2. Filter events by account, and event: allows us to get a list of events performed by a given
-//	  account using the table id.
-//
-// All of these query are essentially taking the entire list of events and allow us to filter them for something
-// more specific: per account, per event type, and per timestamp. All query patterns should support pagination too.
-// We do not support the ability to filter for other addresses, e.g. to answer the a query "has this account interacted
-// with this uniswap router", instead we query for the specific events that would involve that contract. This massively
-// reduces the search space and cost while achieving a similar outcome
+// All of these query are essentially taking the entire list of events and filter on some condition: either we filter
+// for specific chains, specific events, or a specific time.
 
-type Pagination = {
+// Note that we intentionally do not support the ability to filter for addresses - e.g. to answer a query "has
+// this account interacted with the Uniswap router" - and instead resort to querying for specific intents or log events
+// involving that contract. This massively reduces search costs while achieving a similar outcome.
+
+type Opts = {
+	/** Filter for specific chains */
+	chains: Chain[];
+
+	/** Filter for specific events */
+	events: Table[];
+
+	/** Pagination limit */
 	limit: number;
+
+	/** Pagination timestamp */
 	cursor?: string;
+
+	/** Sort ordering */
 	order: "latest" | "reverse";
 };
 
-export async function getEventIdsForAccount(account: `0x${string}`, pagination: Pagination) {
+export async function getEventIdsForAccount(account: `0x${string}`, opts: Opts) {
 	const start = Date.now();
 
 	const client = await createPostgresClient();
 
-	if (pagination.cursor) {
+	const tableIds = opts.events.map((key) => TABLES[key]);
+
+	if (opts.cursor) {
+		const parsedCursor = parseId(opts.cursor);
+
 		const rows = await client
-			.selectDistinct({ event_id: table.event_id })
+			.selectDistinct({
+				chain: table.chain,
+				table_id: table.table_id,
+				block_timestamp: table.block_timestamp,
+
+				tx_index: table.tx_index,
+				log_index: table.log_index,
+				block_number: table.block_number,
+			})
 			.from(table)
-			.where(and(eq(table.account, account), (pagination.order === "latest" ? lt : gt)(table.event_id, pagination.cursor)))
-			.orderBy((pagination.order === "latest" ? desc : asc)(table.event_id))
-			.limit(pagination.limit);
+			.where(
+				and(
+					eq(table.account, account), //
+					inArray(table.chain, opts.chains),
+					inArray(table.table_id, tableIds),
+					(opts.order === "latest" ? lt : gt)(table.block_timestamp, parsedCursor.blockTimestamp),
+					(opts.order === "latest" ? lt : gt)(table.block_number, parsedCursor.blockNumber),
+					(opts.order === "latest" ? lt : gt)(table.tx_index, parsedCursor.txIndex),
+					(opts.order === "latest" ? lt : gt)(table.log_index, parsedCursor.logIndex),
+				),
+			)
+			.orderBy(
+				(opts.order === "latest" ? desc : asc)(table.block_timestamp), //
+				(opts.order === "latest" ? desc : asc)(table.block_number),
+				(opts.order === "latest" ? desc : asc)(table.tx_index),
+				(opts.order === "latest" ? desc : asc)(table.log_index),
+			)
+			.limit(opts.limit);
 
 		logger.debug(`Found ${rows.length} events for account in ${Date.now() - start}ms`);
 
-		return rows.map((result) => result.event_id);
+		return rows.map((result) => {
+			return createId({
+				tableId: result.table_id,
+				chainId: numberToHex(result.chain),
+				txIndex: numberToHex(result.tx_index),
+				logIndex: numberToHex(result.log_index),
+				blockNumber: numberToHex(result.block_number),
+				blockTimestamp: numberToHex(result.block_timestamp),
+			});
+		});
 	}
 
 	const rows = await client
-		.selectDistinct({ event_id: table.event_id })
+		.selectDistinct({
+			chain: table.chain,
+			table_id: table.table_id,
+			block_timestamp: table.block_timestamp,
+
+			tx_index: table.tx_index,
+			log_index: table.log_index,
+			block_number: table.block_number,
+		})
 		.from(table)
-		.where(eq(table.account, account))
-		.orderBy((pagination.order === "latest" ? desc : asc)(table.event_id))
-		.limit(pagination.limit);
+		.where(
+			and(
+				eq(table.account, account), //
+				inArray(table.chain, opts.chains),
+				inArray(table.table_id, tableIds),
+			),
+		)
+		.orderBy(
+			(opts.order === "latest" ? desc : asc)(table.block_timestamp), //
+			(opts.order === "latest" ? desc : asc)(table.block_number),
+			(opts.order === "latest" ? desc : asc)(table.tx_index),
+			(opts.order === "latest" ? desc : asc)(table.log_index),
+		)
+		.limit(opts.limit);
 
 	logger.debug(`Found ${rows.length} events for account in ${Date.now() - start}ms`);
 
-	return rows.map((result) => result.event_id);
+	return rows.map((result) => {
+		return createId({
+			tableId: result.table_id,
+			chainId: numberToHex(result.chain),
+			txIndex: numberToHex(result.tx_index),
+			logIndex: numberToHex(result.log_index),
+			blockNumber: numberToHex(result.block_number),
+			blockTimestamp: numberToHex(result.block_timestamp),
+		});
+	});
 }
