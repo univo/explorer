dfa3d7d2283d53c40b06306765f18189b18fb942
Sam Potter <sam.potter@hotmail.com>
2026-09-09 11:45:05 +1000

    Align colouring

--- src/frames/tx-position/tx-position-client.tsx
+++ src/frames/tx-position/tx-position-client.tsx
@@ -39,7 +39,7 @@ function Header() {
 				</div>
 			</div>
 
-			<div className="flex items-center gap-12 text-sm text-gray-700">
+			<div className="flex items-center gap-12 text-sm text-gray-500">
 				<div className="flex flex-col items-start gap-1">
 					<p>Status</p>
 					<p>Timestamp</p>
