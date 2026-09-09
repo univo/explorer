f41b76042397adbaa9530473cc093e1bf9b54b57
Sam Potter <sam.potter@hotmail.com>
2026-09-09 14:00:28 +1000

    Add test

---
+++ src/indexes/index_account_v4.test.ts
@@ -0,0 +1,35 @@
+import { test } from "vitest";
+
+import { test_getBlock } from "@/tests/utils";
+import { event } from "@/events/intent_native_transfer_v1/event";
+import { getEventIdsForAccount, index_account_v4 } from "./index_account_v4";
+
+test.concurrent("index_account_v4", async ({ expect }) => {
+	const block_number = 10000000;
+
+	const block = await test_getBlock({ chain: 1, block_number });
+
+	const indexes = event.handler(block).flatMap((event) => {
+		return [
+			{ event_id: event.id, account: event.to_address }, //
+			{ event_id: event.id, account: event.from_address },
+		];
+	});
+
+	await index_account_v4.delete(indexes);
+
+	await index_account_v4.upsert(indexes);
+
+	const ids = await getEventIdsForAccount("0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8", {
+		limit: 14,
+		chains: [1],
+		order: "reverse",
+		events: ["intent_native_transfer_v1"],
+	});
+
+	expect(ids).toMatchInlineSnapshot(`
+		[
+		  "5eb01705009896800000ffffff0001000a",
+		]
+	`);
+});
