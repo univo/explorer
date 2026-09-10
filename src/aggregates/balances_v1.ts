89396b011df3a462e6b835ba11752844962174dc
Sam Potter <sam.potter@hotmail.com>
2026-09-10 17:18:40 +1000

    Include erc721 balances

--- src/aggregates/balances_v1.ts
+++ src/aggregates/balances_v1.ts
@@ -1,14 +1,22 @@
 import { aggregate } from "./aggregate";
 
 import { event as log_erc20_transfer_v1 } from "@/events/log_erc20_transfer_v1/event";
+import { event as log_erc721_transfer_v1 } from "@/events/log_erc721_transfer_v1/event";
 
 export const balances_v1 = aggregate({
 	id: "balances_v1",
 
-	events: [log_erc20_transfer_v1],
+	events: [log_erc20_transfer_v1, log_erc721_transfer_v1],
 
 	handlers: {
 		map: (transfer) => {
+			if (transfer.tag === "log_erc721_transfer_v1") {
+				return [
+					[[transfer.to_address, "erc721", transfer.token_address, transfer.token_id].join(":"), 1n],
+					[[transfer.from_address, "erc721", transfer.token_address, transfer.token_id].join(":"), -1n],
+				];
+			}
+
 			return [
 				[[transfer.to_address, "erc20", transfer.token_address].join(":"), BigInt(transfer.quantity)],
 				[[transfer.from_address, "erc20", transfer.token_address].join(":"), -BigInt(transfer.quantity)],
