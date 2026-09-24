30736974631272f12e3e95fd1f062db105ed14a7
Sam Potter <sam.potter@hotmail.com>
2026-09-24 15:55:45 +1000

    chore(migrate): Migrate `log_erc721_transfer_v1` (#91)

--- src/aggregates/balances_v1.ts
+++ src/aggregates/balances_v1.ts
@@ -1,16 +1,16 @@
 import { aggregate } from "./aggregate";
 
 import { event as log_erc20_transfer_v2 } from "@/events/log_erc20_transfer_v2/event";
-import { event as log_erc721_transfer_v1 } from "@/events/log_erc721_transfer_v1/event";
+import { event as log_erc721_transfer_v2 } from "@/events/log_erc721_transfer_v2/event";
 
 export const balances_v1 = aggregate({
 	id: "balances_v1",
 
-	events: [log_erc20_transfer_v2, log_erc721_transfer_v1],
+	events: [log_erc20_transfer_v2, log_erc721_transfer_v2],
 
 	handlers: {
 		map: (transfer) => {
-			if (transfer.tag === "log_erc721_transfer_v1") {
+			if (transfer.tag === "log_erc721_transfer_v2") {
 				return [
 					[[transfer.to_address, "erc721", transfer.token_address, transfer.token_id].join(":"), 1n],
 					[[transfer.from_address, "erc721", transfer.token_address, transfer.token_id].join(":"), -1n],
